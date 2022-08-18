f250c2175577e3d4aa8c5be82c0db7291a3cfec3
Kevin Xu <bobthehoboofhobos@gmail.com>
2022-08-17 22:08:22 -0700

    error page ported

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,30 @@
+const fs = require("fs");
+const NOT_FOUND_PATH = "public/404.html";
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
+
+  eleventyConfig.addPassthroughCopy("./src/img");
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          if (!fs.existsSync(NOT_FOUND_PATH)) {
+            throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+          }
+
+          const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
 
   return {
     dir: {
