808a3ca395edc5c44d2c317aa7cf0d5394eaa062
Kevin Xu <bobthehoboofhobos@gmail.com>
2022-08-19 22:38:18 -0700

    finished up home page, set up art pagination

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,38 @@
 const fs = require("fs");
+const json5 = require("json5");
 const NOT_FOUND_PATH = "public/404.html";
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./src/sass/");
+    eleventyConfig.addWatchTarget("./src/sass/");
 
-  eleventyConfig.addPassthroughCopy("./src/img");
+    eleventyConfig.addPassthroughCopy("./src/img");
 
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, bs) {
+    eleventyConfig.addDataExtension("json5", contents => json5.parse(contents));
 
-        bs.addMiddleware("*", (req, res) => {
-          if (!fs.existsSync(NOT_FOUND_PATH)) {
-            throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
-          }
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function (err, bs) {
 
-          const content_404 = fs.readFileSync(NOT_FOUND_PATH);
-          // Add 404 http status code in request header.
-          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      }
-    }
-  });
+                bs.addMiddleware("*", (req, res) => {
+                    if (!fs.existsSync(NOT_FOUND_PATH)) {
+                        throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+                    }
 
-  return {
-    dir: {
-      input: "src",
-      output: "public",
-    },
-  };
+                    const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+                    // Add 404 http status code in request header.
+                    res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            }
+        }
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        },
+    };
 };
